72fee0accbce780ad2f889eddd98e78011651592
Tyler Phillips <thetylerjp@icloud.com>
2019-04-26 16:45:42 -0400

    Initial Commit

---
+++ AutoProject/AutoProject/AutoProject.h
@@ -0,0 +1,19 @@
+//
+//  AutoProject.h
+//  AutoProject
+//
+//  Created by Tyler P Admin on 4/26/19.
+//  Copyright © 2019 Tyler Phillips. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AutoProject.
+FOUNDATION_EXPORT double AutoProjectVersionNumber;
+
+//! Project version string for AutoProject.
+FOUNDATION_EXPORT const unsigned char AutoProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AutoProject/PublicHeader.h>
+
+
